d72bd2f55934a8677e3a9778db26f33f8ac4a60c
hsthippeswamy <hsthegade@gmail.com>
2025-03-24 15:10:53 +0530

    Add files via upload

---
+++ employee.java
@@ -0,0 +1,25 @@
+package p3;
+
+public class Employee {
+	private String name;
+	private float salary;
+	
+	public void setname(String a) {
+		this.name=a;
+	}
+	public String getname() {
+		return this.name;
+	}
+	public void setSalary(float b) {
+		this.salary=b;
+	}
+	public float getsalary() {
+		return this.salary;
+	}
+  }
+	
+
+	
+
+	
+
